9fb6385aa0bdb186df38b2c7a54bed7a3d934afc
Marcus Eisele <marcus.eisele@gmail.com>
2016-03-22 08:14:22 +0000

    replaced all date analyzing by momentJS

--- app/controllers/requestHandler.server.js
+++ app/controllers/requestHandler.server.js
@@ -3,37 +3,26 @@ var moment = require("moment");
 
 function RequestHandler () {
 
-	var regExOnlyNumbers = /^[0-9]*$/;  
-	//	var months = ["January", "February", "March", "April", "Mai", "June","July","August","September","October","November","December"];
-	
 	var format = "MMMM Do, YYYY";
 	var formatAlt = "MMMM D, YYYY";
-	
-	var formats = [format, formatAlt];
+	var formatTimeStamp = "X"	
+	var formats = [format, formatAlt, formatTimeStamp];
 	
 	this.generateResult = function (req, res) {
 		let query = req.params.query;
-		//check if query consists of only numbers
+		
 		let timestamp;
 		let naturalDate;
-		if(regExOnlyNumbers.test(query)){
-			timestamp = parseInt(query);
-			var mom = moment.unix(timestamp);
-			naturalDate = mom.format("MMMM Do, YYYY");
-			//let temp = new Date(timestamp*1000);
-			//naturalDate = months[temp.getMonth()] + ' ' + temp.getDate() + ', ' + temp.getFullYear();
-		}else{
-			//try to parse date
-			var mom = moment(query, formats);
+		
+		var mom = moment(query, formats);
 			if (mom.isValid()){
-				naturalDate = query;
+				naturalDate = mom.format(format);
 				timestamp = mom.format("X");	
 			}else{
 				naturalDate = null;
 				timestamp = null;
 			}
-			
-		}
+		
 		res.json({'unix': timestamp, 'natural': naturalDate});
 
 	};
